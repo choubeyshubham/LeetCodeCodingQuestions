1c64004ea2e4b9860b190f33a98928faae3c5c5a
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-11-23 15:17:01 +0530

    03 commit

--- src/TwoSum_01.java
+++ src/TwoSum_01.java
@@ -16,12 +16,18 @@ public class TwoSum_01 {
     int[] num={2,7,11,15};
     int target=9;
 
-//        System.out.println(twoSum(num,target));
-        System.out.println(Arrays.toString(twoSum(num,target)));
-        System.out.println("Explaination : Because "+num[0]+" + "+num[1]+" = "+target+", at index"+Arrays.toString(twoSum(num,target)));
+        System.out.println(Arrays.toString(twoSumOn(num,target)));
+        System.out.println("Explaination : Because "+num[0]+" + "+num[1]+" = "+target+", at index"+Arrays.toString(twoSumOn(num,target)));
+
+        System.out.println(Arrays.toString(twoSumOn2(num,target)));
+        System.out.println("Explaination : Because "+num[0]+" + "+num[1]+" = "+target+", at index"+Arrays.toString(twoSumOn2(num,target)));
+
+
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumOn(int[] nums, int target) {
+        // With complexity of O(n)
+
         Map<Integer, Integer> numToIndex = new HashMap<>();
 
         for (int i = 0; i < nums.length; ++i) {
@@ -30,7 +36,21 @@ public class TwoSum_01 {
             numToIndex.put(nums[i], i);
         }
         return new int[0];
-//        throw new IllegalArgumentException();
     }
+
+    public static int[] twoSumOn2(int[] nums, int target) {
+    // With complexity of O(n2)
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+
 
 }
