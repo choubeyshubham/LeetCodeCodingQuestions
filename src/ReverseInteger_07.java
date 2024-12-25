a0a075b9a06d966cb2aeed282b817fff15099cea
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-12-25 22:06:36 +0530

    08 commit

---
+++ src/ReverseInteger_07.java
@@ -0,0 +1,27 @@
+public class ReverseInteger_07 {
+
+    /*
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+Example 1:
+Input: x = 123
+Output: 321
+     */
+
+    public static void main(String[] args) {
+        int x = 123;
+        System.out.println(reverse(x));
+
+    }
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) ? 0 : (int) rev;
+    }
+
+
+}
