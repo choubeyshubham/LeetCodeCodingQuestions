c3df345dec29f4be282ee80a25c974fd2ca067f1
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-12-29 11:32:04 +0530

    10 commit

---
+++ src/PalindromeNumber09.java
@@ -0,0 +1,37 @@
+public class PalindromeNumber09 {
+
+    /*
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+     */
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        long reversed = 0;
+        int y = x;
+
+        while (y > 0) {
+            reversed = reversed * 10 + y % 10;
+            y /= 10;
+        }
+
+        return reversed == x;
+    }
+
+
+
+}
