5fecf935932249316a56d226f503fdeec5c19255
choubeyshubham <shubhamchoubey96@gmail.com>
2025-02-18 15:56:16 +0530

    45 commit

---
+++ src/A21_40/CombinationSum_39.java
@@ -0,0 +1,66 @@
+package A21_40;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum_39 {
+
+
+    /*
+
+Given an array of distinct integers candidates and a target integer target, return a list of all unique
+ combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150
+combinations for the given input.
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+
+     */
+    public static void main(String[] args) {
+        int[] arr = {2,3,6,7};
+        int target = 7;
+        System.out.println(combinationSum(arr, target));
+
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, 0, target, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private static void dfs(int[] candidates, int s, int target, List<Integer> path,
+                     List<List<Integer>> ans) {
+        if (target < 0)
+            return;
+        if (target == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = s; i < candidates.length; ++i) {
+            path.add(candidates[i]);
+            dfs(candidates, i, target - candidates[i], path, ans);
+            path.removeLast();
+        }
+    }
+
+
+
+
+
+}
