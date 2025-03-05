4f04d9eeb43f9fb237b0fc0f1d631427545898ef
choubeyshubham <shubhamchoubey96@gmail.com>
2025-03-05 10:46:58 +0530

    58 commit

---
+++ src/A41_60/LengthofLastWord_58.java
@@ -0,0 +1,36 @@
+package A41_60;
+
+public class LengthofLastWord_58 {
+    /*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ')
+            --i;
+        final int lastIndex = i;
+        while (i >= 0 && s.charAt(i) != ' ')
+            --i;
+
+        return lastIndex - i;
+    }
+
+
+}
