df061f5424f213b8eb9bba0284440688c1036a01
Shubham Choubey <shubhamchoubey96@gmail.com>
2025-03-18 01:58:32 +0530

    68 commit

---
+++ src/A61_80/TextJustification_68.java
@@ -0,0 +1,99 @@
+package A61_80;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextJustification_68 {
+/*
+
+
+Given an array of strings words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line does not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left-justified, and no extra space is inserted between words.
+
+Note:
+
+A word is defined as a character sequence consisting of non-space characters only.
+Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+The input array words contains at least one word.
+
+
+Example 1:
+
+Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+Example 2:
+
+Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be left-justified instead of fully-justified.
+Note that the second line is also left-justified because it contains only one word. */
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> ans = new ArrayList<>();
+        List<StringBuilder> row = new ArrayList<>();
+        int rowLetters = 0;
+
+        for (final String word : words) {
+            // If we place the word in this row, it will exceed the maximum width.
+            // Therefore, we cannot put the word in this row and have to pad spaces
+            // for each word in this row.
+            if (rowLetters + row.size() + word.length() > maxWidth) {
+                final int spaces = maxWidth - rowLetters;
+                if (row.size() == 1) {
+                    // Pad all the spaces after row[0].
+                    for (int i = 0; i < spaces; ++i)
+                        row.get(0).append(" ");
+                } else {
+                    // Evenly pad all the spaces to each word (expect the last one) in
+                    // this row.
+                    for (int i = 0; i < spaces; ++i)
+                        row.get(i % (row.size() - 1)).append(" ");
+                }
+                final String joinedRow =
+                        row.stream().map(StringBuilder::toString).collect(Collectors.joining(""));
+                ans.add(joinedRow);
+                row.clear();
+                rowLetters = 0;
+            }
+            row.add(new StringBuilder(word));
+            rowLetters += word.length();
+        }
+
+        final String lastRow =
+                row.stream().map(StringBuilder::toString).collect(Collectors.joining(" "));
+        StringBuilder sb = new StringBuilder(lastRow);
+        final int spacesToBeAdded = maxWidth - sb.length();
+        for (int i = 0; i < spacesToBeAdded; ++i)
+            sb.append(" ");
+
+        ans.add(sb.toString());
+        return ans;
+    }
+
+
+
+
+
+}
