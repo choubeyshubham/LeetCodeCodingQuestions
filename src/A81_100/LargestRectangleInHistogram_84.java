943e48d258c9d711b8ca48f26281245c4f07b40e
Shubham Choubey <shubhamchoubey96@gmail.com>
2025-04-12 11:45:33 +0530

    85 commit

--- src/A81_100/LargestRectangleInHistogram_84.java
+++ src/A81_100/LargestRectangleInHistogram_84.java
@@ -1,5 +1,8 @@
 package A81_100;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LargestRectangleInHistogram_84 {
     /*
 Given an array of integers heights representing the histogram's bar height where
@@ -16,8 +19,24 @@ The largest rectangle is shown in the red area, which has an area = 10 units.
     public static void main(String[] args) {
         int[] arr={2,1,5,6,2,3};
 
+        System.out.println(largestRectangleArea(arr));
 
 
+    }
+    public static int largestRectangleArea(int[] heights) {
+        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i <= heights.length; ++i) {
+            while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] > heights[i])) {
+                final int h = heights[stack.pop()];
+                final int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                ans = Math.max(ans, h * w);
+            }
+            stack.push(i);
+        }
+
+        return ans;
     }
 
 
---
+++ src/A81_100/MaximalRectangle_85.java
@@ -0,0 +1,64 @@
+package A81_100;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MaximalRectangle_85 {
+    /*
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle
+containing only 1's and return its area.
+
+Example 1:
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+Example 2:
+
+Input: matrix = [["0"]]
+Output: 0
+Example 3:
+
+Input: matrix = [["1"]]
+Output: 1
+
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+    public static int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0)
+            return 0;
+
+        int ans = 0;
+        int[] hist = new int[matrix[0].length];
+
+        for (char[] row : matrix) {
+            for (int i = 0; i < row.length; ++i)
+                hist[i] = row[i] == '0' ? 0 : hist[i] + 1;
+            ans = Math.max(ans, largestRectangleArea(hist));
+        }
+
+        return ans;
+    }
+
+    private static int largestRectangleArea(int[] heights) {
+        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i <= heights.length; ++i) {
+            while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] > heights[i])) {
+                final int h = heights[stack.pop()];
+                final int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                ans = Math.max(ans, h * w);
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+
+
+}
