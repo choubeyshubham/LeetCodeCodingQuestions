76767cc7272d78d63c62a8380078f4c86bb47f07
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-17 09:49:44 +0530

    137 commit

---
+++ src/A121_140/SingleNumber_II_137.java
@@ -0,0 +1,53 @@
+package A121_140;
+
+public class SingleNumber_II_137 {
+    /*
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each element in nums appears exactly three times except for one element which appears once.
+
+     */
+    static void main() {
+
+
+
+    }
+
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; ++i) {
+            int sum = 0;
+            for (final int num : nums)
+                sum += num >> i & 1;
+            sum %= 3;
+            ans |= sum << i;
+        }
+
+        return ans;
+    }
+
+
+
+
+
+
+}
