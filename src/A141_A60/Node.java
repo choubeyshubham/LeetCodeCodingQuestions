93969f98218bb04f7238fa81df6b9f9268fe6e9d
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-26 08:00:02 +0530

    146 commit

---
+++ src/A141_A60/Node.java
@@ -0,0 +1,10 @@
+package A141_A60;
+
+public class Node {
+    public int key;
+    public int value;
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
